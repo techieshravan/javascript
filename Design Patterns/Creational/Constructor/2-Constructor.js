7f1d1b092dd3fd0fc270709060405c643bd8f71e
Shravan <shravan.kasagoni@outlook.com>
2015-09-08 00:48:39 +0530

    moving to folder

---
+++ Design Patterns/Creational/Constructor/2-Constructor.js
@@ -0,0 +1,15 @@
+﻿function Car(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+}
+
+Car.prototype.toString = function () {
+    return this.year + " '" + this.model + "' has done " + this.miles + " miles";
+};
+
+var bentley = new Car("Bentley Continental GT", 2015, 200);
+var porsche = new Car("Porsche Macan S", 2014, 1000);
+
+console.log(bentley.toString());
+console.log(porsche.toString());
